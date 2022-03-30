f21f01be91700f34853f2215401c9dd99abd1fcc
shayanafkane <shayanafkane@gmail.com>
2022-03-30 13:18:35 +0430

    add remaning

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -4,7 +4,10 @@ const AddNote = ({ handleAddNote }) => {
     const [noteText, setNoteText] = useState('');
     const characterLimit = 200;
     const handlechange = (event) => {
-        setNoteText(event.target.value)
+        if (characterLimit - event.target.value.length >= 0) {
+            setNoteText(event.target.value)
+        }
+
     }
     const handleSaveClick = () => {
         if (noteText.trim().length > 0) {
